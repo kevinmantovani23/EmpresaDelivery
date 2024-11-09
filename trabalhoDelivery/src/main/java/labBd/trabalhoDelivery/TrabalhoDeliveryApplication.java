c1d3988f3ee34f39490eb7e07171289cee9cba67
Kevin Mantovani <kevinmantovani23@gmail.com>
2024-11-09 11:20:27 -0300

    primeiro commit

---
+++ trabalhoDelivery/src/main/java/labBd/trabalhoDelivery/TrabalhoDeliveryApplication.java
@@ -0,0 +1,13 @@
+package labBd.trabalhoDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoDeliveryApplication.class, args);
+	}
+
+}
---
+++ trabalhoDelivery/src/test/java/labBd/trabalhoDelivery/TrabalhoDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package labBd.trabalhoDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
